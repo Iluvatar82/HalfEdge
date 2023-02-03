a9939308d2107e1f66ec00aaf51351517ad906cd
Iluvatar82 <spitefr@gmail.com>
2023-02-03 13:45:10 +0100

    Projektdateien hinzufügen.

---
+++ Models/HalfEdge.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace HalfEdge.Models
+{
+    public class HalfEdge<T>
+    {
+        public Vertex<T> Start { get; set; }
+        public Vertex<T> End{ get; set; }
+        public HalfEdge<T> Opposite { get; set; }
+    }
+}
---
+++ Models/Vertex.cs
@@ -0,0 +1,10 @@
+﻿namespace HalfEdge.Models
+{
+    public class Vertex<T>
+    {
+        public T X { get; set; }
+        public T Y { get; set; }
+        public T Z { get; set; }
+        public HalfEdge<T> OutHalfEdges { get; set; }
+    }
+}
