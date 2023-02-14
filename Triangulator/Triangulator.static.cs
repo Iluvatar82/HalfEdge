afe0aef21536bcb6fc4477f9c404b39c395d6812
Iluvatar82 <spitefr@gmail.com>
2023-02-14 09:30:20 +0100

    Triangulation prepared

---
+++ Triangulator/Triangulator.static.cs
@@ -0,0 +1,18 @@
+﻿using HalfEdge.Models;
+
+namespace Triangulator
+{
+    public static class Triangulator<T> where T : struct
+    {
+        public static List<Polygon<T>> Triangulate(Polygon<T> polygon)
+        {
+            //Assumption: all Points are on one Plane!
+
+            //Project to "best" Axis Aligned Plane
+
+            //Triangulate
+
+            throw new NotImplementedException();
+        }
+    }
+}
