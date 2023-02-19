3e5fc7581ea04adccd127565cfc6895d00b3730f
Iluvatar82 <spitefr@gmail.com>
2023-02-19 15:27:03 +0100

    Added IsCCW Extension for (double, double) Tuple

--- Framework/Extensions/CollectionExtensions.cs
+++ Framework/Extensions/CollectionExtensions.cs
@@ -11,5 +11,16 @@ namespace Framework.Extensions
             foreach (var item in enumerable)
                 action(item);
         }
+
+        public static bool IsCCW(this IList<(double X, double Y)> polygon)
+        {
+            int n = polygon.Count;
+            double area = 0.0;
+            for (int p = n - 1, q = 0; q < n; p = q++)
+                area += polygon[p].X * polygon[q].Y - polygon[q].X * polygon[p].Y;
+
+            return area > 0.0f;
+        }
+
     }
 }
