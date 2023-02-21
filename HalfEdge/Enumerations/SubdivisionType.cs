bde5ebadcabe1566f72c4b30b28ebb450af8f56d
Iluvatar82 <spitefr@gmail.com>
2023-02-21 16:27:21 +0100

    Intermediate Commit before Subdivision Modifier implementation

--- HalfEdge/Enumerations/SubdivisionType.cs
+++ HalfEdge/Enumerations/SubdivisionType.cs
@@ -1,9 +1,12 @@
 ﻿namespace HalfEdge.Enumerations
 {
+    /// <summary>
+    /// <see cref="https://www.cs.cmu.edu/afs/cs/academic/class/15462-s14/www/lec_slides/Subdivision.pdf"/>
+    /// </summary>
     public enum SubdivisionType
     {
         CatmullClarke,
         Loop,
-
+        Butterfly
     }
 }
